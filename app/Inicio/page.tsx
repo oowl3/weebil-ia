6ee9344ed8b70c4bdc25c8ffb7ecebbe1fe1c110
Ojeda <ojosue199@gmail.com>
2025-11-22 15:55:46 -0600

    correccion de rutas

--- app/Inicio/page.tsx
+++ app/Inicio/page.tsx
@@ -7,12 +7,12 @@ const Loguin = () => {
     <div>
       <h1>Weebil</h1>
       <h2>Aqui el usuario elige si crea cuenta-ingresa-Continua sin cuenta</h2>
-      <Link href="/Registro" 
+      <Link href="/app/Registro" 
       className="font-bold text-white py-5 px-24sza rounded-xl text-2xl active:scale-95 transition-all duration-150 select-none touch-manipulation shadow-lg hover:shadow-pink-500/50">
         Iniciar secion/Crear cuenta 
       </Link>
       <br />
-      <Link href="/Scaner" 
+      <Link href="/app/analizar_g" 
       className="font-bold text-white py-5 px-24 bg-(--intense-pink) rounded-xl text-2xl active:scale-95 transition-all duration-150 select-none touch-manipulation shadow-lg hover:shadow-pink-500/50">
         Continuar sin cuenta 
       </Link>

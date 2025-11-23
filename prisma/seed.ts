3a4883ce7c40459b399f0ba4f964b8df17797309
londrix <alondrayguadalupe9@gmail.com>
2025-11-23 12:16:44 -0600

    Hospitales agregados

--- prisma/seed.ts
+++ prisma/seed.ts
@@ -6,7 +6,6 @@ async function main() {
   console.log('🌱 Iniciando el seed de la base de datos...');
 
   // 1. LIMPIEZA (Orden inverso para respetar Foreign Keys)
-  // Borramos datos previos para evitar duplicados o errores de constraint
   await prisma.animalDesbloqueado.deleteMany();
   await prisma.analisis.deleteMany();
   await prisma.hospitalAntidoto.deleteMany();
@@ -15,8 +14,6 @@ async function main() {
   await prisma.antidoto.deleteMany();
   await prisma.animal.deleteMany();
   await prisma.user.deleteMany();
-
-  //console.log('🧹 Base de datos limpiada.');
 
   // 2. CREACIÓN DE ANTÍDOTOS
   const sueroPolivalente = await prisma.antidoto.create({
@@ -32,8 +29,6 @@ async function main() {
       descripcion: 'Tratamiento sintomático para picaduras no letales pero dolorosas.',
     },
   });
-
-  //console.log('💉 Antídotos creados.');
 
   // 3. CREACIÓN DE ANIMALES
   const viudaNegra = await prisma.animal.create({
@@ -72,49 +67,437 @@ async function main() {
     },
   });
 
-  //console.log('🕷️ Animales creados.');
-
-  // 4. RELACIONAR ANIMALES CON ANTÍDOTOS (Tabla Intermedia)
+  // 4. RELACIONAR ANIMALES CON ANTÍDOTOS
   await prisma.animalAntidoto.createMany({
     data: [
       { animalId: viudaNegra.id, antidotoId: sueroPolivalente.id },
       { animalId: violinista.id, antidotoId: sueroPolivalente.id },
-      // La saltarina no necesita antídoto específico
     ],
   });
 
-  // 5. CREACIÓN DE HOSPITALES
-  const hospitalCentral = await prisma.hospital.create({
-    data: {
-      nombre: 'Hospital General de la Ciudad',
-      direccion: 'Av. Reforma 123, Centro',
-      telefono: '555-123-4567',
-      latitud: 19.4326, 
-      longitud: -99.1332,
-      ultimaVerificacion: new Date(),
-    },
+  // 5. CREACIÓN DE HOSPITALES - Array con todos los hospitales
+const hospitalesData = [
+  // Hospitales existentes que ya tenías
+  {
+    nombre: 'Hospital integral de Canatlán durango',
+    direccion: 'Independencia y Ramón Corona #40 Zona Centro 34409 Canatlán, Dgo.',
+    telefono: '677-872-0024',
+    latitud: 24.52628,
+    longitud: -104.77492,
+    ultimaVerificacion: new Date(),
+  },
+  {
+    nombre: 'I.S.S.S.T.E Canatlán',
+    direccion: '34455, Morelos 1013, valenzuela 34455 Canatlán, Dgo.',
+    telefono: '677-872-0234',
+    latitud: 24.52125,
+    longitud: -104.77767,
+    ultimaVerificacion: new Date(),
+  },
+  {
+    nombre: 'Cruz roja mexicana Canatlán',
+    direccion: 'Manzanal 102, Soledad Álvarez, 34453 Canatlán, Durango.',
+    telefono: '677-872-0202',
+    latitud: 24.52733,
+    longitud: -104.76657,
+    ultimaVerificacion: new Date(),
+  },
+
+  {
+    nombre: 'I.M.S.S clínica 6',
+    direccion: 'Soledad Álvarez SN, Centro, 34450, entre Alameda y Morelos. Canatlán, Durango',
+    telefono: '677-872-0017',
+    latitud: 24.52342,
+    longitud: -104.77774,
+    ultimaVerificacion: new Date(),
+  },
+  {
+    nombre: 'Canelas Centro de salud',
+    direccion: '34500 Canelas, Durango',
+    telefono: '674-864-0029',
+    latitud: 25.12432,
+    longitud: -106.54528,
+    ultimaVerificacion: new Date(),
+  },
+  {
+    nombre: 'Coneto de comonfort Clínica nogales',
+    direccion: 'Carretera coneteo, 34473 Nogales, Dgo.',
+    telefono: null,
+    latitud: 31.31656,
+    longitud: -110.94847,
+    ultimaVerificacion: new Date(),
+  },
+  {
+    nombre: 'Unidad medica rural IMSS #19 Coneto de Comonfort',
+    direccion: 'Camino A Vizcaino A 500 M De La Telesecundaria , Col. Coneto De Comonfort, C.P. 34475, Durango',
+    telefono: '800-623-2323',
+    latitud: 25.08505,
+    longitud: -104.77583,
+    ultimaVerificacion: new Date(),
+  },
+  {
+    nombre: 'Hospital general de Cuencamé',
+    direccion: 'Camino Pasaje 1, Alamedas, 35800 Cuencamé de Ceniceros, Dgo.',
+    telefono: '671-763-0104',
+    latitud: 24.87395,
+    longitud: -103.70645,
+    ultimaVerificacion: new Date(),
+  },
+  {
+    nombre: 'Centro de salud Cuencamé Dgo.',
+    direccion: 'Primero de Mayo, Alamedas, 35800 Cuencamé de Ceniceros, Dgo.',
+    telefono: null,
+    latitud: 24.86999,
+    longitud: -103.69732,
+    ultimaVerificacion: new Date(),
+  },
+  {
+    nombre: 'Hospital regional de Cuencamé',
+    direccion: 'Carretera A Pasaje Km 1, El Arenal, 35807 Cuencamé de Ceniceros, Dgo.',
+    telefono: '671-763-0104',
+    latitud: 24.87390,
+    longitud: -103.70627,
+    ultimaVerificacion: new Date(),
+  },
+  {
+    nombre: 'Hospital General 450 Durango',
+    direccion: 'Blvd. José María Patoni S/N, El Ciprés, 34206 Durango, Dgo.',
+    telefono: '618-137-3328',
+    latitud: 24.05628,
+    longitud: -104.64162,
+    ultimaVerificacion: new Date(),
+  },
+  {
+    nombre: 'Cruz Roja Mexicana Urgencias Durango',
+    direccion: 'Predio Rustico La Tinaja y Los Lugos, Blvd. José María Patoni, Impregnadora Guadiana, 34279 Durango, Dgo.',
+    telefono: '618-817-3444',
+    latitud: 24.04417,
+    longitud: -104.65600,
+    ultimaVerificacion: new Date(),
+  },
+  {
+    nombre: 'Instituto Mexicano del Seguro Social - Hospital Regional de Zona No. 1',
+    direccion: 'Normal 2011, Predio Canoas, 34076 Durango, Dgo.',
+    telefono: '618-811-9820',
+    latitud: 24.02907,
+    longitud: -104.68263,
+    ultimaVerificacion: new Date(),
+  },
+  {
+    nombre: 'ISSSTE Hospital General Dr. Santiago Ramon y Cajal',
+    direccion: 'Predio Canoas s/n, Colonia Obrera, Silvestre Dorador, 34070 Durango, Dgo.',
+    telefono: '618-811-7513',
+    latitud: 24.02939,
+    longitud: -104.68159,
+    ultimaVerificacion: new Date(),
+  },
+  {
+    nombre: 'Hospital integral Simón Bolívar',
+    direccion: '35950 Gral. Simón Bolívar, Dgo.',
+    telefono: null,
+    latitud: 24.68317,
+    longitud: -103.22704,
+    ultimaVerificacion: new Date(),
+  },
+  {
+    nombre: 'ISSSTE Gómez Palacio',
+    direccion: 'Boulevard Miguel Alemán S/N, Centro, 35000 Gómez Palacio, Dgo.',
+    telefono: '871-714-5547',
+    latitud: 25.55453,
+    longitud: -103.50769,
+    ultimaVerificacion: new Date(),
+  },
+  {
+    nombre: 'Hospital General de Gómez Palacio',
+    direccion: 'De Las Violetas 1152, Rinconadas Bugambilias, 35010 Gómez Palacio, Dgo.',
+    telefono: '871-715-6617',
+    latitud: 25.59147,
+    longitud: -103.48466,
+    ultimaVerificacion: new Date(),
+  },
+  {
+    nombre: 'IMSS Hospital General de Zona Número 51',
+    direccion: 'Calle María Esther Galarza No. 222 Fraccionamiento, Fidel Velásquez, Fidel Velázquez, 35025 Gómez Palacio, Dgo.',
+    telefono: '871-719-6802',
+    latitud: 25.57049,
+    longitud: -103.52182,
+    ultimaVerificacion: new Date(),
+  },
+  {
+    nombre: 'IMSS HGZ No.46',
+    direccion: 'Calz. J. Agustín Castro, Cerro de La Cruz, Revolución, 35000 Gómez Palacio, Dgo.',
+    telefono: '729-293-2131',
+    latitud: 25.55757,
+    longitud: -103.50585,
+    ultimaVerificacion: new Date(),
+  },
+  {
+    nombre: 'Hospital Rural No. 26 IMSS Bienestar',
+    direccion: 'Niños Heroes de Chapultepec SN-S Zona Centro 34700 Guadalupe Victoria Dgo.',
+    telefono: null,
+    latitud: 24.43901,
+    longitud: -104.12197,
+    ultimaVerificacion: new Date(),
+  },
+  {
+    nombre: 'Centro De Salud De Inde',
+    direccion: '35500 Indé, Dgo.',
+    telefono: null,
+    latitud: 25.91578,
+    longitud: -105.22334,
+    ultimaVerificacion: new Date(),
+  },
+  {
+    nombre: 'Hospital general de lerdo',
+    direccion: 'Periférico Gómez-Lerdo Km 12+500 S/N Col. Prudencia Jauregui 35150 Libramiento Rentería S/N Entre Prolongación Durango Y Prolongación Zacatecas',
+    telefono: '871-725-0722',
+    latitud: 25.53191,
+    longitud: -103.53273,
+    ultimaVerificacion: new Date(),
+  },
+  {
+    nombre: 'Cruz Roja Mexicana Delegación Lerdo',
+    direccion: 'C. Gpe. Victoria, Las Brisas, 35153 Lerdo, Dgo.',
+    telefono: '871-298-3911',
+    latitud: 25.52365,
+    longitud: -103.52789,
+    ultimaVerificacion: new Date(),
+  },
+  {
+    nombre: 'IMSS Bermejillo',
+    direccion: 'Carr. Torreon - Jiménez 85, Bermejillo, 35230 Bermejillo, Dgo.',
+    telefono: null,
+    latitud: 25.88378,
+    longitud: -103.62032,
+    ultimaVerificacion: new Date(),
+  },
+  {
+    nombre: 'IMSS 45 Mapimí',
+    direccion: 'Calle G. PACHECO Colonia CENTRO Mapimí, cp 35200',
+    telefono: null,
+    latitud: 25.83302,
+    longitud: -103.84851,
+    ultimaVerificacion: new Date(),
+  },
+  {
+    nombre: 'Hospital integral del mezquital',
+    direccion: 'Calle Principal 325, San Francisco del Mezquital, 34970 Mezquital, Dgo.',
+    telefono: '675-884-4011',
+    latitud: 23.47685,
+    longitud: -104.39214,
+    ultimaVerificacion: new Date(),
+  },
+  {
+    nombre: 'Hospital Integral de Nazas, Dgo',
+    direccion: 'Nazas, Durango (cerrado)',
+    telefono: null,
+    latitud: 25.22500,
+    longitud: -104.10000,
+    ultimaVerificacion: new Date(),
+  },
+  {
+    nombre: 'CRUZ ROJA MEXICANA N. DE DIOS, DGO.',
+    direccion: 'Carretera Panamericana, 34843 Nombre de Dios, Dgo.',
+    telefono: '675-871-7041',
+    latitud: 23.84397,
+    longitud: -104.22520,
+    ultimaVerificacion: new Date(),
+  },
+  {
+    nombre: 'Hospital Las Nieves',
+    direccion: 'Carr. Panamericana KM 333, 35381 Villa las Nieves, Dgo.',
+    telefono: '649-547-0474',
+    latitud: 26.40477,
+    longitud: -105.38120,
+    ultimaVerificacion: new Date(),
+  },
+  {
+    nombre: 'Hospital General El Oro',
+    direccion: 'Sertoma LB, Centro, 35690 Santa María del Oro, Dgo.',
+    telefono: null,
+    latitud: 25.95596,
+    longitud: -105.36644,
+    ultimaVerificacion: new Date(),
+  },
+  {
+    nombre: 'Hospital Integral "Peñon Blanco"',
+    direccion: 'DGO 316, 34764 Puerto Peñasco, Dgo.',
+    telefono: null,
+    latitud: 24.78862,
+    longitud: -104.02269,
+    ultimaVerificacion: new Date(),
+  },
+  {
+    nombre: 'Cruz roja mexicana villa unión',
+    direccion: 'Juárez 603, PUEBLO NUEVO, 34800 Villa Unión, Dgo.',
+    telefono: '675-867-1632',
+    latitud: 23.97458,
+    longitud: -104.05242,
+    ultimaVerificacion: new Date(),
+  },
+  {
+    nombre: 'Hospital Integral Villa Union',
+    direccion: '20 de Noviembre 10, LA COLONIA, 34800 Villa Unión, Dgo.',
+    telefono: '675-867-0020',
+    latitud: 23.98278,
+    longitud: -104.04340,
+    ultimaVerificacion: new Date(),
+  },
+  {
+    nombre: 'Cruz Roja Mexicana delegación El Salto',
+    direccion: 'Ferrocarril s/n, Ferrocarril 628, Juarez, Juárez, 34471 El Salto, Dgo.',
+    telefono: '675-876-0895',
+    latitud: 23.78009,
+    longitud: -105.36198,
+    ultimaVerificacion: new Date(),
+  },
+  {
+    nombre: 'Hospital integral regional El Salto',
+    direccion: 'Durango-Mazatlán Kilómetro 99, El Brillante, 34950 Durango, Dgo. (localidad el salto)',
+    telefono: '675-876-5512',
+    latitud: 23.78819,
+    longitud: -105.34764,
+    ultimaVerificacion: new Date(),
+  },
+  {
+    nombre: 'IMSS HGP Rodeo',
+    direccion: 'Panamericana, 35760 Rodeo, Dgo.',
+    telefono: null,
+    latitud: 25.16976,
+    longitud: -104.55429,
+    ultimaVerificacion: new Date(),
+  },
+  {
+    nombre: 'Hospital integral san juan del rio',
+    direccion: 'La Eta, 34485 San Juan del Río, Dgo.',
+    telefono: '677-866-0378',
+    latitud: 24.78355,
+    longitud: -104.47129,
+    ultimaVerificacion: new Date(),
+  },
+  {
+    nombre: 'Hospital General de Santiago',
+    direccion: 'Baca Ortiz 506, Lomas de San Juan, 34635 Santiago Papasquiaro, Dgo.',
+    telefono: '674-862-3248',
+    latitud: 25.04367,
+    longitud: -105.42297,
+    ultimaVerificacion: new Date(),
+  },
+  {
+    nombre: 'Hospital integral súchil',
+    direccion: 'Av Ferrocarril 301, Centro, 34800 Súchil, Dgo.',
+    telefono: '675-875-0208',
+    latitud: 23.61666,
+    longitud: -103.92410,
+    ultimaVerificacion: new Date(),
+  },
+  {
+    nombre: 'Hospital Integral Tamazula Durango',
+    direccion: 'Gral. Carlos Real Félix, 34580 Tamazula de Victoria, Dgo.',
+    telefono: '667-430-2002',
+    latitud: 24.97223,
+    longitud: -106.96729,
+    ultimaVerificacion: new Date(),
+  },
+  {
+    nombre: 'I.M.S.S. Clínica Número 42',
+    direccion: 'Guanaceví Kilómetro 1, Col del Valle, 35600 Santa Catarina de Tepehuanes, Dgo.',
+    telefono: '674-863-0314',
+    latitud: 25.34053,
+    longitud: -105.72552,
+    ultimaVerificacion: new Date(),
+  },
+  {
+    nombre: 'CENTRO DE SALUD TLAHUALILO',
+    direccion: 'Francisco I. Madero SN-S COMITE DEL PRI, Centro, 35290 Tlahualilo de Zaragoza, Dgo.',
+    telefono: null,
+    latitud: 26.10965,
+    longitud: -103.43989,
+    ultimaVerificacion: new Date(),
+  },
+  {
+    nombre: 'Unidad de medicina familiar No 13 Tlahualilo',
+    direccion: 'Dalias 437, Secc 1339, Las Colonias, 35257 Tlahualilo de Zaragoza, Dgo.',
+    telefono: '872-761-0191',
+    latitud: 26.10182,
+    longitud: -103.44341,
+    ultimaVerificacion: new Date(),
+  },
+  {
+    nombre: 'ISSSTE C.M.F. TLAHUALILO DE ZARAGOZA',
+    direccion: 'Aguascalientes 142, Zaragoza, 35257 Tlahualilo de Zaragoza, Dgo.',
+    telefono: null,
+    latitud: 26.10477,
+    longitud: -103.43906,
+    ultimaVerificacion: new Date(),
+  },
+  {
+    nombre: 'IMSS. Hospital Rural de Solidaridad (Oportunidades)',
+    direccion: 'Carretera a Suchil km. 1.5, José Guadalupe Rodríguez, 34894 Vicente Guerrero, Dgo.',
+    telefono: '675-865-0362',
+    latitud: 23.71938,
+    longitud: -103.97489,
+    ultimaVerificacion: new Date(),
+  },
+  {
+    nombre: 'Cruz Roja Mexicana, Vicente Guerrero',
+    direccion: 'Zona Centro, 34892 Vicente Guerrero, Dgo.',
+    telefono: '675-865-0850',
+    latitud: 23.73798,
+    longitud: -103.98354,
+    ultimaVerificacion: new Date(),
+  },
+  {
+    nombre: 'Centro de Salud Vicente Guerrero',
+    direccion: 'Vicente Guerrero SN-S, San Antonio, 34892 Vicente Guerrero, Dgo.',
+    telefono: '675-865-0158',
+    latitud: 23.73857,
+    longitud: -103.98389,
+    ultimaVerificacion: new Date(),
+  },
+  {
+    nombre: 'Hospital Integral de Nuevo Ideal',
+    direccion: 'Guadalupe Victoria, La Magdalena, 34420 Nuevo Ideal, Dgo.',
+    telefono: null,
+    latitud: 24.88156,
+    longitud: -105.06567,
+    ultimaVerificacion: new Date(),
+  },
+  {
+    nombre: 'Cruz Roja Mexicana Delegación Nuevo Ideal',
+    direccion: 'Av. Miguel Aleman 712, Zona Centro, 34422 Nuevo Ideal, Dgo.',
+    telefono: '677-873-0102',
+    latitud: 24.88586,
+    longitud: -105.07449,
+    ultimaVerificacion: new Date(),
+  },
+  {
+    nombre: 'Instituto Mexicano del Seguro Social Nuevo Ideal',
+    direccion: '20 de Noviembre s/n, Zona Centro, 34410 Nuevo Ideal, Dgo.',
+    telefono: '677-873-0049',
+    latitud: 24.88789,
+    longitud: -105.07386,
+    ultimaVerificacion: new Date(),
+  },
+];
+
+  // Insertar todos los hospitales
+  await prisma.hospital.createMany({
+    data: hospitalesData,
   });
 
-  const clinicaNorte = await prisma.hospital.create({
-    data: {
-      nombre: 'Clínica de Especialidades Norte',
-      direccion: 'Calle Norte 45, Industrial',
-      telefono: '555-987-6543',
-      latitud: 19.4826,
-      longitud: -99.1032,
-      ultimaVerificacion: new Date(),
-    },
-  });
+  // Obtener los IDs de los hospitales recién creados para el inventario
+  const hospitalesCreados = await prisma.hospital.findMany();
+  
+  // 6. INVENTARIO DE HOSPITALES
+  // Asignar antídotos a algunos hospitales (puedes ajustar esta lógica)
+  const inventarioData = hospitalesCreados.map((hospital, index) => [
+    { hospitalId: hospital.id, antidotoId: sueroPolivalente.id, stock: index % 3 === 0 ? 2 : 10 }, // Stock variable
+    { hospitalId: hospital.id, antidotoId: analgesicoFuerte.id, stock: 50 }, // Stock alto para analgésico
+  ]).flat();
 
-  //console.log('🏥 Hospitales creados.');
-
-  // 6. INVENTARIO DE HOSPITALES (Tabla Intermedia)
   await prisma.hospitalAntidoto.createMany({
-    data: [
-      { hospitalId: hospitalCentral.id, antidotoId: sueroPolivalente.id, stock: 10 },
-      { hospitalId: hospitalCentral.id, antidotoId: analgesicoFuerte.id, stock: 50 },
-      { hospitalId: clinicaNorte.id, antidotoId: sueroPolivalente.id, stock: 2 }, // Stock bajo
-    ],
+    data: inventarioData,
   });
 
   // 7. CREAR USUARIO DE PRUEBA
@@ -127,43 +510,37 @@ async function main() {
     },
   });
 
-  //console.log('👤 Usuario demo creado.');
-
-  // 8. SIMULAR ANIMALES DESBLOQUEADOS POR EL USUARIO
+  // 8. SIMULAR ANIMALES DESBLOQUEADOS
   await prisma.animalDesbloqueado.create({
     data: {
       usuarioId: usuarioDemo.id,
-      animalId: saltarina.id, // El usuario ya encontró una saltarina
+      animalId: saltarina.id,
     },
   });
 
-  // 9. SIMULAR UN ANÁLISIS (HISTORIAL DE ESCANEO)
+  // 9. SIMULAR UN ANÁLISIS
   await prisma.analisis.create({
     data: {
       usuarioId: usuarioDemo.id,
       rutaImagen: '/uploads/analisis/scan_001.jpg',
       latitudUsuario: 19.4326,
       longitudUsuario: -99.1332,
-      
-      // Resultado de la IA
       animalDetectadoId: viudaNegra.id,
       esVenenosoDetectado: true,
       confianzaIA: 0.98,
       descripcionIA: 'Alta probabilidad de Latrodectus mactans por patrón abdominal.',
       primerosAuxiliosIA: 'Busque atención médica inmediata. No aplique torniquetes.',
-      
-      // Recomendaciones del sistema
       antidotoSugeridoId: sueroPolivalente.id,
-      hospitalRecomendadoId: hospitalCentral.id,
+      hospitalRecomendadoId: hospitalesCreados[0].id, // Usar el primer hospital
     },
   });
 
-  //console.log('✅ Seed completado exitosamente.');
+  console.log(`✅ Seed completado exitosamente. ${hospitalesData.length} hospitales creados.`);
 }
 
 main()
   .catch((e) => {
-    //console.error('❌ Error durante el seed:', e);
+    console.error('❌ Error durante el seed:', e);
     process.exit(1);
   })
   .finally(async () => {

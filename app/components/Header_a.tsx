1fb762dc6115da130d0e23e5986a0209727508d1
Ojeda <ojosue199@gmail.com>
2025-11-24 10:46:31 -0600

    avance

--- app/components/Header_a.tsx
+++ app/components/Header_a.tsx
@@ -4,20 +4,23 @@ import Link from "next/link";
 
 export default function Header_a() {
   return (
-   <header
-    className="w-full bg-white shadow-sm rounded-b-[120px] border-t-4 border-black flex items-center justify-center py-20">
-  <div className="flex items-center gap-4">
-    <Logo />
-    <h1 className="font-bold flex items-center">
-      <span className="text-(--intense-pink) text-5xl">W</span>
-      <span className="text-(--black-deep) text-5xl">e</span>
-      <span className="text-(--black-deep) text-5xl">e</span>
-      <span className="text-(--black-deep) text-5xl">b</span>
-      <span className="text-(--green-light) text-5xl">i</span>
-      <span className="text-(--green-light) text-5xl">l</span>
-    </h1>
-  </div>
-</header>
-
+    <Link href="/Inicio" className="block w-full group">
+      <header
+        className="w-full bg-white shadow-sm rounded-b-[120px] border-t-4 border-black flex items-center justify-center py-20 
+        transition-transform duration-300 ease-out group-hover:scale-[1.02] group-active:scale-95 cursor-pointer"
+      >
+        <div className="flex items-center gap-4">
+          <Logo />
+          <h1 className="font-bold flex items-center select-none">
+            <span className="text-(--intense-pink) text-5xl">W</span>
+            <span className="text-(--black-deep) text-5xl">e</span>
+            <span className="text-(--black-deep) text-5xl">e</span>
+            <span className="text-(--black-deep) text-5xl">b</span>
+            <span className="text-(--green-light) text-5xl">i</span>
+            <span className="text-(--green-light) text-5xl">l</span>
+          </h1>
+        </div>
+      </header>
+    </Link>
   );
 }

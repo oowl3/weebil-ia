a7d30253678aaad2bbc900e06e66c50064964849
Ojeda <ojosue199@gmail.com>
2025-11-25 10:11:32 -0600

    avance tiktok

--- middleware.ts
+++ middleware.ts
@@ -47,6 +47,6 @@ export default withAuth(
 
 export const config = {
   matcher: [
-    "/((?!_next/static|_next/image|favicon.ico|.*\\.(?:svg|avif|png|jpg|jpeg|gif|webp)$).*)",
+    "/((?!_next/static|_next/image|favicon.ico|.*\\.(?:svg|avif|png|jpg|jpeg|gif|webp|txt)$).*)",
   ],
 };
